e8960901e0247644d15157f72d1b53922f97f73b
NLOU5 <35761265+NLOU5@users.noreply.github.com>
2022-11-22 21:13:09 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './css/App.css';
-
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, shrink-to-fit=no"></meta>
 
 function App() {
 
